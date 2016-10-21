1caad57e2908782e4c30cc92ad6fbe3be2388236
nasiar2003 <nasiar2003@yahoo.fr>
2016-10-21 17:27:19 +0200

    creazione del model e voce poi implentazione dei metodi nel model

--- src/it/polito/tdp/rubrica/model/Model.java
+++ src/it/polito/tdp/rubrica/model/Model.java
@@ -20,16 +20,49 @@ public class Model {
 		this.voci= new ArrayList<>();
 	}
 	
-	
-	public void addVoce(Voce c){
+	/**
+	 * Aggiunge una nuova voce dentro la lista delle voce, prima controlla se è presente 
+	 * dentro la lista ritorna {@code false} .
+	 * @param voce  da aggiungere dentro la lista
+	 * @return  ritorna {@code true} se la voce è stata aggiunta correttamente
+	 */
+	public boolean addVoce(Voce voce){
+		if(voci.contains(voce)){
+			return false;
+		}
+		
+		voci.add(voce);
+		
+		
+		return true;
 		
 	}
-	
-	
+	/**
+	 * cerca il nome dentro la lista se non trova ritorna {@code null} se non è presente
+	 * 
+	 * @param nome la stringa nome da cercare
+	 * @return ritorna la {@code Voce}
+	 * 
+	 * 
+	 * 
+	 */
 	public Voce findVoceByNome(String nome){
+		for(int i=0; i<voci.size();++i){
+			if(voci.get(i).getNome().equals(nome)){
+				  
+				return voci.get(i);
+			}
+		}
 		
+		return null;
 		
 		
 	}
 
+	
+	
+
+	
+	
+	
 }
--- src/it/polito/tdp/rubrica/model/Voce.java
+++ src/it/polito/tdp/rubrica/model/Voce.java
@@ -7,10 +7,72 @@ package it.polito.tdp.rubrica.model;
  */
 
 public class Voce {
+	
 	private String cognome;
 	private String nome;
 	private String telefono;
 	private String email;
+	public Voce(String cognome, String nome, String telefono, String email) {
+		super();
+		this.cognome = cognome;
+		this.nome = nome;
+		this.telefono = telefono;
+		this.email = email;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cognome == null) ? 0 : cognome.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voce other = (Voce) obj;
+		if (cognome == null) {
+			if (other.cognome != null)
+				return false;
+		} else if (!cognome.equals(other.cognome))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
 	
 
+	
 }
