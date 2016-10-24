04c12fb218713b9c07916fd0d83345653adc2dbc
nasiar2003 <nasiar2003@yahoo.fr>
2016-10-24 18:06:36 +0200

    cancellazione dell'elemento cognome

--- src/it/polito/tdp/rubrica/model/Voce.java
+++ src/it/polito/tdp/rubrica/model/Voce.java
@@ -8,49 +8,50 @@ package it.polito.tdp.rubrica.model;
 
 public class Voce {
 	
-	private String cognome;
+	
 	private String nome;
 	private String telefono;
 	private String email;
-	public Voce(String cognome, String nome, String telefono, String email) {
+	
+	public Voce(String nome, String telefono, String email) {
 		super();
-		this.cognome = cognome;
 		this.nome = nome;
 		this.telefono = telefono;
 		this.email = email;
 	}
-	public String getCognome() {
-		return cognome;
-	}
-	public void setCognome(String cognome) {
-		this.cognome = cognome;
-	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getTelefono() {
 		return telefono;
 	}
+
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((cognome == null) ? 0 : cognome.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -60,11 +61,6 @@ public class Voce {
 		if (getClass() != obj.getClass())
 			return false;
 		Voce other = (Voce) obj;
-		if (cognome == null) {
-			if (other.cognome != null)
-				return false;
-		} else if (!cognome.equals(other.cognome))
-			return false;
 		if (nome == null) {
 			if (other.nome != null)
 				return false;
@@ -73,6 +69,9 @@ public class Voce {
 		return true;
 	}
 	
-
+	
+	
+	
+	
 	
 }
